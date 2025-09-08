9ae7a3bcec05f771eec82b6b37ab8b64d57afc8b
turat <turat.samar@gmail.com>
2025-09-09 02:12:18 +0900

    add  merged code

--- main.c
+++ main.c
@@ -1,16 +1,9 @@
 #include <stdint.h>
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 int fun(int addrs);
 int function(void); //else
-=======
+
 int main(void){
     return 0;
 }
->>>>>>> 6bc5a98 (add function)
-=======
-int main(void){
-    return 0;
-}
->>>>>>> 1ed2f19 (add main() function)
+
